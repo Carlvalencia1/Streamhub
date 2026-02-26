62ee267c8d3b541c1d3b8cc7e4cc2618291fe355
Joaquín Gómez <joaquingomez@MacBook-Air-de-Joaquin.local>
2026-02-26 06:04:03 -0600

    fix

--- app/src/main/java/com/valencia/streamhub/features/jsonplaceholder/domain/repositories/PostsRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.valencia.demo.features.jsonplaceholder.domain.repositories
-
-import com.valencia.demo.features.jsonplaceholder.domain.entities.Posts
-
-interface PostsRepository {
-        suspend fun getPosts(): List<Posts>
-}
