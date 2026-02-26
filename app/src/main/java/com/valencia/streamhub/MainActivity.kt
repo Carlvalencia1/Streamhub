6baa7a6acdc0969a24393f0af81a0f30e3d10c1b
Joaquín Gómez <joaquingomez@MacBook-Air-de-Joaquin.local>
2026-02-26 06:04:03 -0600

    fix

--- app/src/main/java/com/valencia/streamhub/MainActivity.kt
+++ app/src/main/java/com/valencia/streamhub/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.valencia.streamhub.core.navigation.AppNavGraph
 import com.valencia.streamhub.core.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,8 +16,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
+                val navController = rememberNavController()
+                AppNavGraph(navController = navController)
             }
-                }
+        }
     }
 }
-
