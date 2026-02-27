6967b709e87e2d38ca06ed9d16fd8c64ec5f4868
Carlvalencia1 <233428@ids.edu.upchiapas>
2026-02-26 19:44:39 -0600

    IU

--- app/src/main/java/com/valencia/streamhub/core/di/Qualifiers.kt
+++ app/src/main/java/com/valencia/streamhub/core/di/Qualifiers.kt
@@ -4,13 +4,4 @@ import javax.inject.Qualifier
 
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
-annotation class RickAndMortyRetrofit
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class JsonPlaceHolderRetrofit
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
 annotation class StreamhubRetrofit
-
